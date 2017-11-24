bd4f66f8f2de786d6de0bb8aeb5067dfa975e1c1
anders <anders.ahp@gmail.com>
2017-11-24 11:59:42 +0100

    Kan gemme arf

--- app/src/main/java/com/example/ulrich/contextapp/MainActivity.java
+++ app/src/main/java/com/example/ulrich/contextapp/MainActivity.java
@@ -2,16 +2,12 @@ package com.example.ulrich.contextapp;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.example.ulrich.contextapp.widgets.AccelerometerWidget;
@@ -24,42 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int requestCode = 0;
-        int grantResults[] = null;
-        //if you dont have required permissions ask for it (only required for API 23+)
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},requestCode);
-
-
-        onRequestPermissionsResult(requestCode,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},grantResults);
-
-
-    }
-
-    @Override // android recommended class to handle permissions
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    Log.d("permission", "granted");
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.uujm
-                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
-
-                    //app cannot function without this permission for now so close it...
-                    onDestroy();
-                }
-                return;
-            }
-
-            // other 'case' line to check fosr other
-            // permissions this app might request
-        }
+        AccelerometerWidget widget = new AccelerometerWidget((SensorManager)getSystemService(Context.SENSOR_SERVICE));
     }
 }
